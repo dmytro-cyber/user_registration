f49c5e471af2ad09d878b430b10fb649f06b7b5c
94nj111 <94nj111@gmail.com>
2025-03-19 17:18:32 +0200

    add cors

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.v1.routers.auth import router as auth_router
 from api.v1.routers.user import router as user_router
 from core.setup import create_roles
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="My Async FastAPI Project")
@@ -9,6 +10,15 @@ app = FastAPI(title="My Async FastAPI Project")
 @app.on_event("startup")
 async def on_startup():
     await create_roles()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/api/v1", tags=["Auth"])
 app.include_router(user_router, prefix="/api/v1", tags=["User"])
