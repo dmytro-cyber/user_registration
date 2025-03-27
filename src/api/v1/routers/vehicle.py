a0f5f9185a0d655caec398ab5bd9735bc1a9a70c
94nj111 <94nj111@gmail.com>
2025-03-27 14:56:14 +0200

    add filters car endpoint

--- src/api/v1/routers/vehicle.py
+++ src/api/v1/routers/vehicle.py
@@ -3,6 +3,9 @@ from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
+from sqlalchemy import Integer, String
+from sqlalchemy.sql import text
+from sqlalchemy.orm.attributes import InstrumentedAttribute
 
 from db.session import get_db
 from models.vehicle import CarModel
@@ -19,16 +22,42 @@ async def get_cars(
     request: Request,
     page: int = Query(1, ge=1),
     page_size: int = Query(10, ge=1, le=100),
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(get_db),
+    mileage_min: int = Query(None),
+    mileage_max: int = Query(None),
 ) -> CarListResponseSchema:
-    total_count = await db.scalar(select(func.count()).select_from(CarModel))
+    query = select(CarModel).options(selectinload(CarModel.photos))
+    
+    filters = request.query_params
+    print("FILTERS: ", filters)
+
+    if filters:
+        for field, value in filters.items():
+            if hasattr(CarModel, field) and value is not None:
+                column_attr: InstrumentedAttribute = getattr(CarModel, field)
+
+                column_type = column_attr.property.columns[0].type
+                
+                if isinstance(column_type, String):
+                    query = query.filter(column_attr.ilike(f"%{value}%"))
+                elif isinstance(column_type, Integer):
+                    try:
+                        query = query.filter(column_attr == int(value))
+                    except ValueError:
+                        print(f"Помилка: Неможливо привести {field} до числа")
+                else:
+                    query = query.filter(column_attr == value)
+    
+    if mileage_min is not None:
+        query = query.filter(CarModel.mileage >= mileage_min)
+    if mileage_max is not None:
+        query = query.filter(CarModel.mileage <= mileage_max)
+    
+    total_count = await db.scalar(select(func.count()).select_from(query.subquery()))
     total_pages = (total_count + page_size - 1) // page_size
     
     result = await db.execute(
-        select(CarModel)
-        .options(selectinload(CarModel.photos))
-        .offset((page - 1) * page_size)
-        .limit(page_size)
+        query.offset((page - 1) * page_size).limit(page_size)
     )
     cars = result.scalars().all()
     
