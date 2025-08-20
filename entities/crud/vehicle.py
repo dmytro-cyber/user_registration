d4e796cea632339e626423d2775bab7b13ec5c24
Dmytro Havrylenko <94nj111@gmail.com>
2025-08-20 11:35:49 +0300

    fix

--- entities/crud/vehicle.py
+++ entities/crud/vehicle.py
@@ -242,24 +242,24 @@ async def save_vehicle_with_photos(vehicle_data: CarCreateSchema, ivent: str, db
                 else:
                     vehicle.recommendation_status_reasons += f"{vehicle.transmision};"
 
-            # query = select(FilterModel).where(
-            #     FilterModel.make == vehicle_data.make,
-            #     or_(
-            #         FilterModel.model == vehicle_data.model,
-            #         FilterModel.model.is_(None)
-            #     ),
-            #     FilterModel.year_from <= vehicle_data.year,
-            #     FilterModel.year_to >= vehicle_data.year,
-            #     FilterModel.odometer_max >= vehicle_data.mileage
-            # )
-            # filter_ex = await db.execute(query)
-            # filter_res = filter_ex.scalars().one_or_none()
-            # if filter_res:
-            #     vehicle.relevance = RelevanceStatus.ACTIVE
-            #     to_parse = True
-            # else:
-            #     vehicle.relevance = RelevanceStatus.IRRELEVANT
-            #     to_parse = False
+            query = select(FilterModel).where(
+                FilterModel.make == vehicle_data.make,
+                or_(
+                    FilterModel.model == vehicle_data.model,
+                    FilterModel.model.is_(None)
+                ),
+                FilterModel.year_from <= vehicle_data.year,
+                FilterModel.year_to >= vehicle_data.year,
+                FilterModel.odometer_max >= vehicle_data.mileage
+            )
+            filter_ex = await db.execute(query)
+            filter_res = filter_ex.scalars().one_or_none()
+            if filter_res:
+                vehicle.relevance = RelevanceStatus.ACTIVE
+                to_parse = True
+            else:
+                vehicle.relevance = RelevanceStatus.IRRELEVANT
+                to_parse = False
             if vehicle_data.condition_assessments:
                 for assessment in vehicle_data.condition_assessments:
                     if assessment.issue_description != "Unknown":
