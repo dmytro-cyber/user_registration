e90cd9658bc3bc2eed5f916d39bf5d8361a506cc
admin <admin@admins-MacBook-Air.local>
2025-08-29 15:24:16 +0300

    fix

--- entities/tasks/task.py
+++ entities/tasks/task.py
@@ -457,14 +457,14 @@ def update_car_bids() -> Dict[str, Any]:
                     else:
                         pass
 
-                car.predicted_total_investments = (car.sum_of_investments or 0.0) + (car.current_bid or 0)
+                # car.predicted_total_investments = (car.sum_of_investments or 0.0) + (car.current_bid or 0)
                 if car.predicted_total_investments and car.avg_market_price:
                     car.predicted_roi = (
-                        (car.avg_market_price - car.predicted_total_investments)
-                        / car.predicted_total_investments
+                        (car.avg_market_price - (car.sum_of_investments + (car.current_bid or 0)))
+                        / (car.sum_of_investments + (car.current_bid or 0))
                         * 100.0
                     )
-                    car.predicted_profit_margin = car.avg_market_price - car.predicted_total_investments
+                    car.predicted_profit_margin = car.avg_market_price - (car.sum_of_investments + (car.current_bid or 0))
                 updated += 1
 
             db.commit()
