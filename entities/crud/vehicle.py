7e95ee7927ddcd92e9ca602c6cab3a3d9f2aaf1e
Dmytro Havrylenko <94nj111@gmail.com>
2025-09-03 14:43:35 +0300

    fil

--- entities/crud/vehicle.py
+++ entities/crud/vehicle.py
@@ -123,24 +123,24 @@ async def save_vehicle_with_photos(vehicle_data: CarCreateSchema, ivent: str, db
     try:
         existing_vehicle = await get_vehicle_by_vin(db, vehicle_data.vin, 1)
         if existing_vehicle:
-            if existing_vehicle.relevance == RelevanceStatus.ACTIVE:
-                to_parse = False
-            elif existing_vehicle.relevance == RelevanceStatus.ARCHIVAL and ivent == "update":
-                query = select(FilterModel).where(
-                    FilterModel.make == vehicle_data.make,
-                    FilterModel.model == vehicle_data.model,
-                    FilterModel.year_from <= vehicle_data.year,
-                    FilterModel.year_to >= vehicle_data.year,
-                    FilterModel.odometer_max >= vehicle_data.mileage
-                )
-                filter_ex = await db.execute(query)
-                filter_res = filter_ex.scalars().one_or_none()
-                if filter_res:
-                    existing_vehicle.relevance = RelevanceStatus.ACTIVE
-                    to_parse = True
-                else:
-                    existing_vehicle.relevance = RelevanceStatus.IRRELEVANT
-                    to_parse = False
+            # if existing_vehicle.relevance == RelevanceStatus.ACTIVE:
+            #     to_parse = False
+            # elif existing_vehicle.relevance == RelevanceStatus.ARCHIVAL and ivent == "update":
+            #     query = select(FilterModel).where(
+            #         FilterModel.make == vehicle_data.make,
+            #         FilterModel.model == vehicle_data.model,
+            #         FilterModel.year_from <= vehicle_data.year,
+            #         FilterModel.year_to >= vehicle_data.year,
+            #         FilterModel.odometer_max >= vehicle_data.mileage
+            #     )
+            #     filter_ex = await db.execute(query)
+            #     filter_res = filter_ex.scalars().one_or_none()
+            #     if filter_res:
+            #         existing_vehicle.relevance = RelevanceStatus.ACTIVE
+            #         to_parse = True
+            #     else:
+            #         existing_vehicle.relevance = RelevanceStatus.IRRELEVANT
+            #         to_parse = False
 
             
             logger.info(f"Vehicle with VIN {vehicle_data.vin} already exists. Updating data...")
@@ -224,7 +224,8 @@ async def save_vehicle_with_photos(vehicle_data: CarCreateSchema, ivent: str, db
 
             await db.commit()
 
-            return to_parse
+            # return to_parse
+            return False
 
         else:
             vehicle = CarModel(
@@ -300,7 +301,8 @@ async def save_vehicle_with_photos(vehicle_data: CarCreateSchema, ivent: str, db
                 await save_sale_history(vehicle_data.sales_history, vehicle.id, db)
 
             await db.commit()
-            return to_parse
+            # return to_parse
+            return False
 
     except IntegrityError as e:
         if "unique constraint" in str(e).lower() and "vin" in str(e).lower():
