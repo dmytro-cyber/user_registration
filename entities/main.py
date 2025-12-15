0d286c72c27d0d4bc8c51c4a19fb84cbd58ead33
Dmytro Havrylenko <94nj111@gmail.com>
2025-12-15 14:17:01 +0200

    Revert "fix"
    This reverts commit bc7a57468354d491d9d7d32558802ef7b1e818af.

--- entities/main.py
+++ entities/main.py
@@ -21,10 +21,10 @@ app = FastAPI(
 )
 
 
-# @app.on_event("startup")
-# async def on_startup():
-#     await create_roles()
-#     await import_us_zips_from_csv()
+@app.on_event("startup")
+async def on_startup():
+    await create_roles()
+    await import_us_zips_from_csv()
     # celery_app.send_task(
     #     "tasks.task.update_fees",
     #     queue="car_parsing_queue",
