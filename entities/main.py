f81e2d47087992a00c49e1553afbbf5aa388134e
admin <admin@admins-MacBook-Air.local>
2026-02-03 15:10:38 +0200

    -startup

--- entities/main.py
+++ entities/main.py
@@ -21,13 +21,13 @@ app = FastAPI(
 )
 
 
-@app.on_event("startup")
-async def on_startup():
+# @app.on_event("startup")
+# async def on_startup():
     # await create_roles()
-    celery_app.send_task(
-        "tasks.task.update_fees",
-        queue="car_parsing_queue",
-    )
+    # celery_app.send_task(
+    #     "tasks.task.update_fees",
+    #     queue="car_parsing_queue",
+    # )
     # await import_us_zips_from_csv()
 
 # @app.on_event("startup")
