7b836f8bf94dac89b828998ed1f067459bb0944a
Dmytro Havrylenko <94nj111@gmail.com>
2026-06-18 18:05:15 +0300

    fix

--- entities/utils.py
+++ entities/utils.py
@@ -1,7 +1,7 @@
 from sqlalchemy import func, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.vehicle import CarInventoryInvestmentsModel, CarInventoryModel
+from models.vehicle import CarInventoryInvestmentsModel, CarInventoryModel, CarInventoryStatus
 
 
 async def update_inventory_financials(db: AsyncSession, inventory_id: int):
@@ -13,12 +13,12 @@ async def update_inventory_financials(db: AsyncSession, inventory_id: int):
     sums_by_type = dict(result.all())
 
     update_values = {
-        "parts_cost": sums_by_type.get("PARTS", 0),
-        "maintenance": sums_by_type.get("MAINTENANCE", 0),
-        "auction_fee": sums_by_type.get("AUCTION_FEE", 0),
-        "transportation": sums_by_type.get("TRANSPORTATION", 0),
-        "labor": sums_by_type.get("LABOR", 0),
-        "additional_costs": sums_by_type.get("ADDITIONAL_COSTS", 0),
+        "parts_cost": sums_by_type.get(CarInventoryStatus.PARTS, 0),
+        "maintenance": sums_by_type.get(CarInventoryStatus.MAINTENANCE, 0),
+        "auction_fee": sums_by_type.get(CarInventoryStatus.AUCTION_FEE, 0),
+        "transportation": sums_by_type.get(CarInventoryStatus.TRANSPORTATION, 0),
+        "labor": sums_by_type.get(CarInventoryStatus.LABOR, 0),
+        "additional_costs": sums_by_type.get(CarInventoryStatus.ADDITIONAL_COSTS, 0),
     }
 
     await db.execute(update(CarInventoryModel).where(CarInventoryModel.id == inventory_id).values(**update_values))
